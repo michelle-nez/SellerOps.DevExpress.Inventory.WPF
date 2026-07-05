05da0937c7a9b9a6111f6eddd49c62b60e81bbcd
Michelle Nesbitt <wallplatecity@yahoo.com>
2026-07-05 16:44:17 -0400

    Created the Models

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SellerOps.DevExpress.Inventory.Wpf.Models
+{
+    public sealed class Product : ViewModelBase
+    {
+        private string _sku = string.Empty;
+
+        private string _title = string.Empty;
+
+        private string _marketplace = string.Empty;
+
+        private int _quantity;
+
+        private decimal _price;
+
+        private ProductStatus _status;
+
+        private DateTime _lastUpdated = DateTime.Today;
+    }
+}
---
+++ Models/ProductStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SellerOps.DevExpress.Inventory.Wpf.Models
+{
+    public enum ProductStatus
+    {
+        Active,
+        LowStock,
+        NeedsReview,
+        Draft, 
+        Discontinued
+    }
+}
