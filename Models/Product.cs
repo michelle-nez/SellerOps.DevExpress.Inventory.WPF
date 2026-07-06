1b45b084036c1acfc81e8af06055a07bc902fdc0
Michelle Nesbitt <wallplatecity@yahoo.com>
2026-07-06 09:54:00 -0400

    FInished the RelayCommand module

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SellerOps.DevExpress.Inventory.Wpf.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SellerOps.DevExpress.Inventory.Wpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SellerOps.DevExpress.Inventory.Wpf.ViewModels
+{
+    public sealed class MainViewModel
+    {
+        private Product? _selectedProduct;
+        private int _newProductNumber = 1000;
+
+        public  ObservableCollection<Product> Products { get; } = new();
+        
+
+
+    }
+}
