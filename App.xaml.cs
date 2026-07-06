e3e9c2c7edbedb6778a85b3b0a91ae65d65f6258
Michelle Nesbitt <wallplatecity@yahoo.com>
2026-07-06 12:00:20 -0400

    Added dependency in App.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using System.Configuration;
-using System.Data;
 using System.Windows;
+using DevExpress.Xpf.Core;
 
 namespace SellerOps.DevExpress.Inventory.Wpf;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        ApplicationThemeHelper.ApplicationThemeName = Theme.Office2019ColorfulName;
+        base.OnStartup(e);
+    }
 }
 
