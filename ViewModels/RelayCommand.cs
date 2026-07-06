03451981ef23926aa27d8b5283ec4bfcef07d0fc
Michelle Nesbitt <wallplatecity@yahoo.com>
2026-07-06 10:17:03 -0400

    Add ViewModelBase, RelayCommand, and MainViewModel for the inventory gri

--- ViewModels/RelayCommand.cs
+++ ViewModels/RelayCommand.cs
@@ -5,34 +5,24 @@ using System.Windows.Input;
 
 namespace SellerOps.DevExpress.Inventory.Wpf.ViewModels
 {
-    public class RelayCommand : ICommand
+    public sealed class RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) : ICommand
     {
         public event EventHandler? CanExecuteChanged;
-        private readonly Action<object?> _execute;
-        private readonly Func<object?, bool> _canExecute;
-
-        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
-        {
-
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            _canExecute = canExecute;
-        }
-
 
         public bool CanExecute(object? parameter)
         {
-            return _canExecute?.Invoke(parameter) ?? true;
-
+            return canExecute?.Invoke(parameter) ?? true;
         }
 
         public void Execute(object? parameter)
         {
-            _execute(parameter);
+            execute(parameter);
         }
 
         public void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
-}
+    }
+
 }
