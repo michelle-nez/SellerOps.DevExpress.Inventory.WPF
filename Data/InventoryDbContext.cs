0271de75dfbb8ca29e2ec3322302a0e5125192d1
Michelle Nesbitt <wallplatecity@yahoo.com>
2026-07-09 10:14:09 -0400

    Add EF Core SQLite provider and InventoryDbContext

---
+++ Data/InventoryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.CodeParser;
+using Microsoft.EntityFrameworkCore;
+using SellerOps.DevExpress.Inventory.Wpf.Models;
+
+namespace SellerOps.DevExpress.Inventory.Wpf.Data
+{
+    public sealed class InventoryDbContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=inventory.db");
+        }
+    }
+}
